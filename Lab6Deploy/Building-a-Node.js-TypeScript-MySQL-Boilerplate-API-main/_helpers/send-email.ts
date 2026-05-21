2c6bfe2d06c140f12514f6f3744ac50d79234d06
Jess Getaruelas <jessgetaruelas@gmail.com>
2026-05-21 17:46:46 +0800

    Switch to Resend for email and update send-email.ts

--- Lab6Deploy/Building-a-Node.js-TypeScript-MySQL-Boilerplate-API-main/_helpers/send-email.ts
+++ Lab6Deploy/Building-a-Node.js-TypeScript-MySQL-Boilerplate-API-main/_helpers/send-email.ts
@@ -3,4 +3,15 @@ import config from '../config';
 export default async function sendEmail({to,subject,html, from = config.emailFrom}: any){
   const transporter = nodemailer.createTransport(config.smtpOptions);
   await transporter.sendMail({from, to, subject, html});  
+}import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export default async function sendEmail({ to, subject, html, from = 'onboarding@resend.dev' }: any) {
+    await resend.emails.send({
+        from,
+        to,
+        subject,
+        html
+    });
 }
